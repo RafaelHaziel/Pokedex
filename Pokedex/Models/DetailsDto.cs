89cfdf94ca3e10d68b87a6ea22455a2a3a1d57f8
Rafael Haziel <hazieldefato@gmail.com>
2023-03-13 08:12:47 -0300

    Criada as Classes do projeto

--- Pokedex/Models/DetailsDto.cs
+++ Pokedex/Models/DetailsDto.cs
@@ -1,12 +1,10 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
 namespace Pokedex.Models
 {
     public class DetailsDto
     {
-        
+        public Pokemon Prior { get; set; }
+        public Pokemon Current { get; set; }
+        public Pokemon Next { get; set; }
+        public List<Tipo> Tipos { get; set; }
     }
 }
--- Pokedex/Models/PokedexDto.cs
+++ Pokedex/Models/PokedexDto.cs
@@ -1,12 +1,8 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
 namespace Pokedex.Models
 {
     public class PokedexDto
     {
-        
+        public List<Tipo> Tipos { get; set; }
+        public List<Pokemon> Pokemons { get; set; }
     }
 }
