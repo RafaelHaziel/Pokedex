5dfe0321790ee8aac303dc0d647c2f5d8f5297da
Rafael Haziel <hazieldefato@gmail.com>
2023-04-03 07:58:47 -0300

    Terminado o projeto, feita as devidas alterações no site.js, index.cshtml e _Layout.cshtml.

--- Pokedex/Controllers/HomeController.cs
+++ Pokedex/Controllers/HomeController.cs
@@ -2,7 +2,9 @@
 using Microsoft.AspNetCore.Mvc;
 using Pokedex.Models;
 using Pokedex.Services;
+
 namespace Pokedex.Controllers;
+
 public class HomeController : Controller
     {
     private readonly ILogger<HomeController> _logger;
